a217aeeb217e7fa281b421b8462a5850ca91a192
SowahCore <SilverCraft226@gmail.com>
2020-12-22 13:05:50 +0100

    HeadsTails function

---
+++ bot.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const {token, prefix} = require("./config");
+
+bot.on("ready", () => {
+    console.log(bot.user.tag + " prêt à pif");
+});
+
+bot.on("message", msg => {
+    if (msg.content.startsWith(prefix) && msg.member) {
+        commandProcess(msg);
+    }
+})
+
+function commandProcess(msg){
+    let rawCommand = msg.content;
+    let fullCommand = rawCommand.substr(prefix.length);
+    let splitCommand = fullCommand.split(' ');
+    splitCommand = splitCommand.filter(function(e){return e});
+    let primaryCommand = splitCommand[0];
+
+    switch (primaryCommand.toLowerCase()) {
+        case 'between':
+            commandBetween(msg);
+            break;
+        case 'for':
+            commandFor(msg);
+            break;
+        case 'headsTails':
+            commandHeadsTails(msg);
+            break;
+        default:
+            msg.reply("fdp.");
+    }
+}
+
+function commandBetween(msg) {
+    msg.reply("pas fait (Between)").catch(console.error);
+}
+
+function commandFor(msg) {
+    msg.reply("pas fait (For)").catch(console.error);
+}
+
+function commandHeadsTails(msg) {
+    let result = Math.floor(Math.random() * Math.floor(2));
+    if (result === 0) msg.reply("c'est face").catch(console.error);
+    else if (result === 1) msg.reply("c'est pile").catch(console.error);
+    else msg.reply("c'est bugger").catch(console.error);
+}
+
+bot.login(token).catch(console.error);
