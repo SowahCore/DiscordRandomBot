66424c798a27b1641182e8f4f189a6d7c3d632f5
SowahCore <SilverCraft226@gmail.com>
2020-12-22 14:14:49 +0100

    commandFor

--- bot.js
+++ bot.js
@@ -19,7 +19,6 @@ function commandProcess(msg){
     splitCommand = splitCommand.filter(function(e){return e});
     let primaryCommand = splitCommand[0];
 
-    console.log(primaryCommand.toLowerCase());
     switch (primaryCommand.toLowerCase()) {
         case 'between':
             commandBetween(msg);
@@ -40,13 +39,23 @@ function commandBetween(msg) {
 }
 
 function commandFor(msg) {
-    msg.reply("commande non faite").catch(console.error);
+
+    let rawCommand = msg.content;
+    let fullCommand = rawCommand.substr(prefix.length);
+    let splitCommand = fullCommand.split(' ');
+    splitCommand = splitCommand.filter(function(e){return e});
+    let number = parseInt(splitCommand[1]);
+
+    let result = Math.floor(Math.random() * Math.floor(number+1));
+    msg.channel.send("le resultat est : " + result).catch(console.error);
 }
 
 function commandHeadsTails(msg) {
     let result = Math.floor(Math.random() * Math.floor(2));
-    if (result === 0) msg.reply("c'est face").catch(console.error);
-    else if (result === 1) msg.reply("c'est pile").catch(console.error);
+    let pile = "https://cdn.discordapp.com/attachments/790915412762165268/790925318042550282/pile.gif";
+    let face = "https://cdn.discordapp.com/attachments/790915412762165268/790926059016814602/face.gif";
+    if (result === 0) msg.channel.send(face).catch(console.error);
+    else if (result === 1) msg.channel.send(pile).catch(console.error);
     else msg.reply("c'est buggé").catch(console.error);
 }
 
