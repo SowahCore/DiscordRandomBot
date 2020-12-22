3927cafc3b052bcd1721e4ca89f6a3e5c3498e49
SowahCore <SilverCraft226@gmail.com>
2020-12-22 14:22:01 +0100

    commandBetween

--- bot.js
+++ bot.js
@@ -35,7 +35,21 @@ function commandProcess(msg){
 }
 
 function commandBetween(msg) {
-    msg.reply("commande non faite").catch(console.error);
+
+    let rawCommand = msg.content;
+    let fullCommand = rawCommand.substr(prefix.length);
+    let splitCommand = fullCommand.split(' ');
+    splitCommand = splitCommand.filter(function(e){return e});
+    let number1 = parseInt(splitCommand[1]);
+    let number2 = parseInt(splitCommand[2]);
+
+    let result;
+
+    if (number1 > number2) result = getRandomIntInclusive(number2, number1);
+    else if (number1 < number2) result = getRandomIntInclusive(number1, number2);
+    else result = number1;
+
+    msg.channel.send("le resultat est : " + result).catch(console.error);
 }
 
 function commandFor(msg) {
@@ -57,6 +71,12 @@ function commandHeadsTails(msg) {
     if (result === 0) msg.channel.send(face).catch(console.error);
     else if (result === 1) msg.channel.send(pile).catch(console.error);
     else msg.reply("c'est buggé").catch(console.error);
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
 bot.login(token).catch(console.error);
